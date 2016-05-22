d05512e920fda8c11d53612771adc20a57070065
Stephen Walker <stephendwalker+github@gmail.com>
2016-05-22 14:41:58 -0400

    This week's update, add LEDE project, switch to secure patchwork links

---
+++ uscan/js/sort-lede.js
@@ -0,0 +1,24 @@
+"use strict";
+var i;
+var areas = ["packageBoot", "packageDevel", "packageFirmware", "packageKernel",
+             "packageKernelLantiq", "packageLibs", "packageNetworkConfig",
+             "packageNetworkIpv6", "packageNetworkServices",
+             "packageNetworkUtils", "packageSystem", "packageUtils",
+             "toolchain", "tools"];
+
+for (i in areas) {
+  if (areas.hasOwnProperty(i)) {
+    if (areas[i] === "packageKernelLantiq") {
+      var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "s", "", "s", "s"];
+    } else if (areas[i] === "toolchain") {
+      var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "", "s", "s"];
+    } else if (areas[i] === "tools") {
+      var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "", "s", "s"];
+    } else {
+      var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "s", "s", "", "s", "s"];
+    }
+
+    var TSort_Initial = ["0D"];
+    tsRegister();
+  }
+}
