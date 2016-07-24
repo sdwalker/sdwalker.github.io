494ce11aa78808bcc986c6b4f6f9221ce8fb3f97
Stephen Walker <stephendwalker+github@gmail.com>
2016-07-24 16:35:52 -0400

    This week's update, switch to GitHub for OpenWrt, drop the OpenWrt desktop, efl, lxde, oldpackages, phone, xfce and xorg feeds

--- uscan/js/sort.js
+++ uscan/js/sort.js
@@ -4,23 +4,17 @@ var areas = ["packageBoot", "packageDevel", "packageFirmware", "packageKernel",
              "packageKernelLantiq", "packageLibs", "packageNetworkConfig",
              "packageNetworkIpv6", "packageNetworkServices",
              "packageNetworkUtils", "packageSystem", "packageUtils",
-             "packageFeedsDesktop", "packageFeedsEfl", "packageFeedsLxde",
-             "packageFeedsManagement", "packageFeedsOldpackages",
-             "packageFeedsPackages", "packageFeedsPhone", "packageFeedsRouting",
-             "packageFeedsTelephony", "packageFeedsXfce", "packageFeedsXorg",
-             "toolchain", "tools"];
+             "packageFeedsManagement", "packageFeedsPackages",
+             "packageFeedsRouting", "packageFeedsTelephony", "toolchain",
+             "tools"];
 
 for (i in areas) {
   if (areas.hasOwnProperty(i)) {
-    if (areas[i] === "packageFeedsEfl" ||
-        areas[i] === "packageFeedsPhone" ||
-        areas[i] === "packageKernelLantiq") {
+    if (areas[i] === "packageKernelLantiq") {
       var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "s", "", "", "s", "s"];
     } else if (areas[i] === "toolchain") {
       var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "", "", "s", "s"];
-    } else if (areas[i] === "packageFeedsLxde" ||
-               areas[i] === "packageFeedsXfce" ||
-               areas[i] === "tools") {
+    } else if (areas[i] === "tools") {
       var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "", "", "s", "s"];
     } else {
       var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "s", "s", "", "", "s", "s"];
