b694b8f1743edaf6e34669d1f9b99ab96a447d82
Stephen Walker <stephendwalker+github@gmail.com>
2025-03-16 14:46:48 -0400

    This week's update

--- uscan/js/sort.js
+++ uscan/js/sort.js
@@ -1,18 +1,17 @@
 "use strict";
 var i;
 var areas = ["packageBoot", "packageDevel", "packageFirmware",
-             "packageFirmwareLayerscape", "packageKernel",
-             "packageKernelLantiq", "packageLibs", "packageNetworkConfig",
-             "packageNetworkIpv6", "packageNetworkServices",
-             "packageNetworkUtils", "packageNetworkUtilsLayerscape",
-             "packageSystem", "packageUtils", "packageFeedsPackages",
-             "packageFeedsRouting", "packageFeedsTelephony", "toolchain",
-             "tools"];
+             "packageFirmwareLantiq", "packageFirmwareLayerscape",
+             "packageKernel", "packageKernelLantiq", "packageLibs",
+             "packageNetworkConfig", "packageNetworkIpv6",
+             "packageNetworkServices", "packageNetworkUtils",
+             "packageNetworkUtilsLayerscape", "packageSystem", "packageUtils",
+             "packageFeedsPackages", "packageFeedsRouting",
+             "packageFeedsTelephony", "toolchain", "tools"];
 
 for (i in areas) {
   if (areas.hasOwnProperty(i)) {
-    if (areas[i] === "packageDevel" ||
-        areas[i] === "packageFirmwareLayerscape" ||
+    if (areas[i] === "packageFirmwareLayerscape" ||
         areas[i] === "packageNetworkUtilsLayerscape" ||
         areas[i] === "tools") {
       var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "s", "", "", "s", "s"];
