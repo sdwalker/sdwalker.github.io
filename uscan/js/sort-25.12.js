2ffb9e4bc685ca1760c005e4f426ab5f9697ea63
Stephen Walker <stephendwalker+github@gmail.com>
2026-03-08 17:59:31 -0400

    This week's update, stop updating 23.05, add 25.12

---
+++ uscan/js/sort-25.12.js
@@ -0,0 +1,28 @@
+"use strict";
+var i;
+var areas = ["packageBoot", "packageDevel", "packageFirmware",
+             "packageFirmwareLantiq", "packageFirmwareLayerscape",
+             "packageKernel", "packageKernelLantiq", "packageLibs",
+             "packageNetworkConfig", "packageNetworkIpv6",
+             "packageNetworkServices", "packageNetworkUtils",
+             "packageNetworkUtilsLayerscape", "packageSystem", "packageUtils",
+             "packageFeedsPackages", "packageFeedsRouting",
+             "packageFeedsTelephony", "packageFeedsVideo", "toolchain",
+             "tools"];
+
+for (i in areas) {
+  if (areas.hasOwnProperty(i)) {
+    if (areas[i] === "packageFirmwareLayerscape" ||
+        areas[i] === "packageNetworkUtilsLayerscape" ||
+        areas[i] === "tools") {
+      var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "s", "", "", "s", "s"];
+    } else if (areas[i] === "toolchain") {
+      var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "", "", "s", "s"];
+    } else {
+      var TSort_Data = [areas[i] + "Table", "s", "s", "s", "s", "s", "s", "s", "s", "", "", "s", "s"];
+    }
+
+    var TSort_Initial = ["0D"];
+    tsRegister();
+  }
+}
